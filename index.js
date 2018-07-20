aa83a61468b617d7c0f61832b82f00985b23106a
kumanoko <noeleon930.hater.1@gmail.com>
2018-07-20 22:47:11 +0800

    Complete doc

--- index.js
+++ index.js
@@ -8,40 +8,44 @@ export const CreateEthereumKeyJson = createEthereumKeyJson;
 export const DecryptEthereumKeyJson = decryptEthereumKeyJson;
 export const SignTransaction = signTransaction;
 
-if (!(typeof self === "undefined")) {
-  self.eth_key_lib_CreatePrivateKeyHexString = createPrivateKeyHexString;
-  self.eth_key_lib_CreateEthereumKeyJson = createEthereumKeyJson;
-  self.eth_key_lib_DecryptEthereumKeyJson = decryptEthereumKeyJson;
-  self.eth_key_lib_SignTransaction = signTransaction;
+// Code sample:
+export function TestEthKeyLib() {
+  const pk = createPrivateKeyHexString();
+  const keyJson = createEthereumKeyJson("!!321iamastrongpassphrase123!!", pk);
+  const decryptedWalletObj = decryptEthereumKeyJson(
+    keyJson,
+    "!!321iamastrongpassphrase123!!"
+  );
+
+  console.log("The private key is", pk);
+  console.log("The key json is", JSON.stringify(keyJson, null, 2));
+  console.log(
+    "The decrypted wallet obj from the key json is",
+    JSON.stringify(decryptedWalletObj, null, 2)
+  );
+  console.log(
+    "The decrypted privatekey from the key json is",
+    "0x" + decryptedWalletObj.privateKeyBuffer.toString("hex")
+  );
+  console.log(
+    "The signed transaction hex string is",
+    signTransaction(decryptedWalletObj.privateKeyBuffer, {
+      chainId: 42,
+      data: "0x",
+      gas: "0x5208",
+      gasPrice: "0x3b9aca00",
+      nonce: "0xed",
+      to: "0x1211ef4E91607766a19e544a2F8d0CA68837cAd0",
+      value: "0xc12dc63fa970000"
+    })
+  );
 }
 
-// Code sample:
-//
-// const pk = createPrivateKeyHexString();
-// const keyJson = createEthereumKeyJson("!!321iamastrongpassphrase123!!", pk);
-
-// console.log("The private key is", pk);
-// console.log("The key json is", JSON.stringify(keyJson, null, 2));
-// console.log(
-//   "The decrypted privatekey from the key json is",
-//   "0x" +
-//     decryptEthereumKeyJson(
-//       keyJson,
-//       "!!321iamastrongpassphrase123!!"
-//     ).privateKeyBuffer.toString("hex")
-// );
-// console.log(
-//   signTransaction(
-//     decryptEthereumKeyJson(keyJson, "!!321iamastrongpassphrase123!!")
-//       .privateKeyBuffer,
-//     {
-//       chainId: 42,
-//       data: "0x",
-//       gas: "0x5208",
-//       gasPrice: "0x3b9aca00",
-//       nonce: "0xed",
-//       to: "0x1211ef4E91607766a19e544a2F8d0CA68837cAd0",
-//       value: "0xc12dc63fa970000"
-//     }
-//   )
-// );
+if (!(typeof self === "undefined")) {
+  self.EthKeyLibBrowser = {};
+  self.EthKeyLibBrowser.CreatePrivateKeyHexString = createPrivateKeyHexString;
+  self.EthKeyLibBrowser.CreateEthereumKeyJson = createEthereumKeyJson;
+  self.EthKeyLibBrowser.DecryptEthereumKeyJson = decryptEthereumKeyJson;
+  self.EthKeyLibBrowser.SignTransaction = signTransaction;
+  self.EthKeyLibBrowser.TestEthKeyLib = TestEthKeyLib;
+}
